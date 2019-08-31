ad550d2f42c11d8c831fd0b30132fc34250230df
Minhwan Kim <azurelysium@gmail.com>
2019-08-31 20:52:14 +0900

    initial version

---
+++ examples/dot-button/user_defined.py
@@ -0,0 +1,26 @@
+import datetime
+from button.tasks import Task, CommandExecTask
+
+class DisplayDateTask(CommandExecTask):
+    def __init__(self):
+        super().__init__('date', 'display current datetime', 'date')
+
+class MultipleCommandsTask(CommandExecTask):
+    def __init__(self):
+        script = '''
+        MYVAR="hello"
+        echo $MYVAR, world
+        '''
+        super().__init__('script', 'executes multiple commands', script)
+
+class Epoch2DateTask(Task):
+    def __init__(self):
+        super().__init__('epoch2date', 'convert epoch to date')
+
+    def add_arguments(self, parser):
+        parser.add_argument('epoch', type=int, help='unix epoch value')
+
+    def run(self, parsed):
+        print(datetime.datetime.fromtimestamp(parsed.epoch).isoformat())
+
+tasks = [DisplayDateTask, MultipleCommandsTask, Epoch2DateTask]
