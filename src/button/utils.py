331b617706c623cba10cb3ba0565df80f27f513e
Minhwan Kim <azurelysium@gmail.com>
2019-09-03 18:11:26 +0900

    fix utils

--- src/button/utils.py
+++ src/button/utils.py
@@ -7,15 +7,10 @@ def execute_shell_command(command, preserve_output=False):
     p = subprocess.Popen(shlex.split(cmd),
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
-    while True:
-        retval = p.poll()
-        if retval is not None:
-            break
-        output = p.stdout.readline()
-        if output:
-            line = output.decode('utf-8').rstrip()
-            print(line)
-            if preserve_output:
-                lines.append(line)
-        time.sleep(0.001)
+    for line in iter(p.stdout.readline, b''):
+        line_utf8 = line.decode('utf-8').rstrip()
+        print(line_utf8)
+        if preserve_output:
+            lines.append(line_utf8)
+    retval = p.poll()
     return retval, lines
